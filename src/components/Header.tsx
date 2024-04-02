b5a1ad506899e34b03bdde4853ef3b1a602fe3c1
Rodrigo Serrasqueiro <rodrigoserrasqueiro@gmail.com>
2024-04-02 15:27:35 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import nlwUniteIcon from '../assets/nlw-unite-icon.svg';
+
+export function Header() {
+  return (
+    <div className="flex items-center gap-5 py-2">
+      <img src={nlwUniteIcon} alt="logo icon" />
+
+      <nav className="flex items-center gap-5">
+        <a className="font-medium text-sm text-zinc-300" href="">Eventos</a>
+        <a className="font-medium text-sm text" href="">Participantes</a>
+      </nav>
+    </div>
+  )
+}
